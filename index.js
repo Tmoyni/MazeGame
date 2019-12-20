87e4b572ed9d9fdffffb920590011153d37dd8f0
MarioBland <gabedotgutierrez@gmail.com>
2019-12-20 13:23:16 -0500

    gabe move without flicker

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ let squareX = 18;
 let squareY = 18;
 
 function drawPaddle() { //draws square
- 
+  makeWhite(squareX-5, squareY-5, squareWidth+10, squareHeight+10);
   context.beginPath();
   context.rect(squareX, squareY, squareWidth, squareHeight); //first two are position, second two is x/y size
   context.fillStyle = "#0095DD";
   context.fill();
   context.closePath();
-  
+  // makeWhite(squareX, squareY, 20, 20);
 }
 
 
 
 function drawMazeAndRectangle(rectX, rectY) { //what is rectx and recty?
     // context.clearRect(0, 0, canvas.width, canvas.height)
-    makeWhite(0, 0, canvas.width, canvas.height);
+    // makeWhite(0, 0, canvas.width, canvas.height);
     var mazeImg = new Image();
     mazeImg.onload = function () {
         context.drawImage(mazeImg, 0, 0);
@@ -36,10 +36,9 @@ function drawMazeAndRectangle(rectX, rectY) { //what is rectx and recty?
         context.fillStyle = '#00FF00';
         context.fill();
     };
-    
-    console.log(squareX, squareY, squareWidth, squareHeight)
     drawPaddle()
-    mazeImg.src = "https://freesvg.org/img/simplemaze.png";
+    console.log(squareX, squareY, squareWidth, squareHeight)
+    mazeImg.src = "assets/simplemaze.png";
     // requestAnimationFrame(drawMazeAndRectangle) //gotta make this work tommorrow
 }
 
@@ -59,6 +58,8 @@ canvas.addEventListener("mousemove", function(event){ //mouse control here
 
   
 
-// drawMazeAndRectangle(425, 3);
+
 // context.clearRect(0, 0, canvas.width, canvas.height)
-setInterval(drawMazeAndRectangle,1000/60)
+// setInterval(drawPaddle,1000/60)
+// drawMazeAndRectangle(425, 3);
+setInterval(drawMazeAndRectangle, 1000/60)
