9bbc1b1dcb2bcf929ad98d6ea0a86479ee3bce50
MarioBland <gabedotgutierrez@gmail.com>
2019-12-24 10:21:11 -0500

    Two layers created

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let mazeWidth = 600;
 let mazeHeight = 600;
 let intervalVar;
 
+//making a second layer
+let imgCanvas = document.getElementById("imgcanvas");
+let imgContext = imgCanvas.getContext("2d");
+
 //making a game piece square 
 let squareHeight = 10;
 let squareWidth = 10;
@@ -13,33 +17,30 @@ let squareX = 18;
 let squareY = 18;
 
 function drawPaddle() { //draws square
-  makeWhite(squareX-5, squareY-5, squareWidth+10, squareHeight+10);
+  // makeWhite(squareX-5, squareY-5, 20, 20);
+  context.clearRect(0, 0, canvas.width, canvas.height)
   context.beginPath();
   context.rect(squareX, squareY, squareWidth, squareHeight); //first two are position, second two is x/y size
   context.fillStyle = "#0095DD";
   context.fill();
   context.closePath();
-  // makeWhite(squareX, squareY, 20, 20);
 }
 
 
 
-function drawMazeAndRectangle(rectX, rectY) { //what is rectx and recty?
-    // context.clearRect(0, 0, canvas.width, canvas.height)
-    // makeWhite(0, 0, canvas.width, canvas.height);
-    var mazeImg = new Image();
+function drawMazeAndRectangle(rectX, rectY) { //original maze and player piece
+    let mazeImg = new Image();
     mazeImg.onload = function () {
         context.drawImage(mazeImg, 0, 0);
-        context.beginPath();
-        context.arc(403, 590, 7, 0, 2 * Math.PI, false); 
-        context.closePath();
-        context.fillStyle = '#00FF00';
-        context.fill();
+        // context.beginPath();
+        // context.arc(403, 590, 7, 0, 2 * Math.PI, false); //this makes the goal circle, but I have it erasing the whole board so... :(
+        // context.closePath();
+        // context.fillStyle = '#00FF00';
+        // context.fill();
     };
     drawPaddle()
-    console.log(squareX, squareY, squareWidth, squareHeight)
     mazeImg.src = "assets/simplemaze.png";
-    // requestAnimationFrame(drawMazeAndRectangle) //gotta make this work tommorrow
+    
 }
 
 function makeWhite(x, y, w, h) {
@@ -50,16 +51,26 @@ function makeWhite(x, y, w, h) {
     context.fillStyle = "white";
     context.fill();
 }
+
 canvas.addEventListener("mousemove", function(event){ //mouse control here
   canvas.style.cursor = "none"
   squareX = event.clientX 
   squareY = event.clientY  
   })
 
-  
+function drawImage(){ //second layer code
+  let mazeImg = new Image();
+    mazeImg.onload = function () {
+        imgContext.drawImage(mazeImg, 0, 0);
+        // imgContext.beginPath();
+        // imgContext.arc(403, 590, 7, 0, 2 * Math.PI, false); 
+        // imgContext.closePath();
+        // imgContext.fillStyle = '#00FF00';
+        // imgContext.fill();
+    };
+        mazeImg.src = "assets/simplemaze.png";
 
+}
 
-// context.clearRect(0, 0, canvas.width, canvas.height)
-// setInterval(drawPaddle,1000/60)
-// drawMazeAndRectangle(425, 3);
-setInterval(drawMazeAndRectangle, 1000/60)
+drawImage()
+setInterval(drawMazeAndRectangle,1000/60)
