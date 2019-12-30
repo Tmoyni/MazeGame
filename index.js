edb02f40e1e3fabf72d73145095f58a593a797ee
MarioBland <gabedotgutierrez@gmail.com>
2019-12-30 17:53:11 -0500

    some collision

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ let mazeWidth = 600;
 let mazeHeight = 600;
 let intervalVar;
 
+
+function getScores(){
+  return fetch('http://localhost:3000/api/v1/scores')
+    .then(function (response) {return response.json() })
+    .then(function (scores) {
+      let ul = document.getElementById("scores")
+      scores.forEach(function (score){
+        let scoreLi = document.createElement("li")
+        scoreLi.innerText = `
+        Name: ${score.player} - Score: ${score.score}
+        `
+        ul.appendChild(scoreLi)
+      })
+    })
+}
+
+getScores()
+
 //making a second layer
 let imgCanvas = document.getElementById("imgcanvas");
 let imgContext = imgCanvas.getContext("2d");
@@ -22,39 +40,32 @@ let scoreCounter = document.getElementById("score")
 
 //draws gameplay avatar/game piece/ etc
 function drawPaddle() { 
-
   context.clearRect(0, 0, canvas.width, canvas.height)
   context.beginPath();
   context.rect(squareX, squareY, squareWidth, squareHeight); //first two are position, second two is x/y size
   context.fillStyle = "#0095DD";
   context.fill();
   context.closePath();
-  
 }
 
 //creating score counter
 let scoreHolder = document.createElement("div")
 scoreHolder.innerText = `Seconds: ${scoreNum}`
 scoreCounter.appendChild(scoreHolder)
-
-
 function drawMazeAndRectangle(rectX, rectY) { //original maze and player piece
     let mazeImg = new Image();
-    // mazeImg.crossOrigin = "Anonymous";
     mazeImg.onload = function () {
-        // context.drawImage(mazeImg, 0, 0);
+        context.drawImage(mazeImg, 0, 0);
         // context.beginPath();
-        // context.arc(403, 590, 7, 0, 2 * Math.PI, false); //this makes the goal circle, but I have it erasing the whole board so... :(
-        // context.closePath();
+        // context.arc(403, 590, 7, 0, 2 * Math.PI, false); //this makes the goal circle, but I have it erasing the whole board so... :        // context.closePath();
         // context.fillStyle = '#00FF00';
         // context.fill();
-
     }
     scoreNum += 1
-    console.log(scoreNum)
     scoreHolder.innerText = `Seconds: ${(scoreNum/60).toFixed(2)}` //updates our score here. currently in seconds. (stretch goal, countdown time meter?)
-    mazeImg.src = "assets/simplemaze.png"
-    canMoveTo(squareX, squareY)
+    mazeImg.crossOrigin = "Anonymous";
+    mazeImg.src = "https://cors-anywhere.herokuapp.com/https://freesvg.org/img/simplemaze.png"
+    
     drawPaddle()
 }
 
@@ -67,13 +78,7 @@ function makeWhite(x, y, w, h) {
     context.fill();
 }
 
-canvas.addEventListener("mousemove", function(event){ //mouse control here
-  canvas.style.cursor = "none"
-  squareX = event.clientX 
-  squareY = event.clientY - 30 // have to reposition the y value now that we moved  
-  })
-
-function drawImage(){ //second layer code
+  function drawImage(){ //second layer code
   let mazeImg = new Image();
   // mazeImg.crossOrigin = "Anonymous";
     mazeImg.onload = function () {
@@ -84,8 +89,9 @@ function drawImage(){ //second layer code
         imgContext.fillStyle = '#00FF00';
         imgContext.fill();
     };
-        mazeImg.src = "assets/simplemaze.png";
-
+        mazeImg.crossOrigin = "Anonymous";
+        mazeImg.src = "https://cors-anywhere.herokuapp.com/https://freesvg.org/img/simplemaze.png"
+        ;
 }
 
 // can move code
@@ -106,16 +112,20 @@ function canMoveTo(squareX, squareY) {
       }
   }
   else {
-      canMove = 0;
+      canMove = 1;
   }
-  // console.log(canMove)
   return canMove;
-
-  
 }
+
+canvas.addEventListener("mousemove", function(event){ //mouse control here
+    movingAllowed = canMoveTo(squareX,squareY);
+    console.log(movingAllowed)
+    canvas.style.cursor = "none"
+        if (movingAllowed === 1){
+    squareX = event.clientX 
+    squareY = event.clientY - 30 // have to reposition the y value now that we moved  
+        }
+    })
+
 drawImage()
 setInterval(drawMazeAndRectangle,1000/60) //score is done on a fixed 60fps timer (more resource intensive) however, this allows us to append our score counter to this, causing it to go up by 1 every frame and then do math to make into seconds.
-
-/*
-Access to image at from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
-*/
